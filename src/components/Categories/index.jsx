288fb7094600be5be25a32704a35d0673882d32a
Artemy-sweden <ghosteagle2020@mail.ru>
2024-06-06 20:37:32 +0300

    add: category choice logic added

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Categories() {
-  const [activeCategory, setActiveCategory] = React.useState(0);
+  const categoryArray = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const [activeCategory, setActiveCategory] = useState(0);
+
+  const onClickCategory = (category) => {
+    setActiveCategory(category);
+  };
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categoryArray.map((category, index) => (
+          <li
+            className={activeCategory === index ? "active" : ""}
+            onClick={() => onClickCategory(index)}
+          >
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   );
